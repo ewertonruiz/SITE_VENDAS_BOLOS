636405b91d66e2a1db13132a195e30e6caa81efe
ewerton <ewertonruiz@gmail.com>
2023-03-06 23:04:54 -0500

    Criação das Models do Backend

---
+++ backend/boloscaseiros/src/main/java/br/com/ewertoncorporation/boloscaseiros/BoloscaseirosApplication.java
@@ -0,0 +1,13 @@
+package br.com.ewertoncorporation.boloscaseiros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoloscaseirosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoloscaseirosApplication.class, args);
+	}
+
+}
---
+++ backend/boloscaseiros/src/main/java/br/com/ewertoncorporation/boloscaseiros/model/TipoUsuario.java
@@ -0,0 +1,26 @@
+package br.com.ewertoncorporation.boloscaseiros.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@Entity
+
+public class TipoUsuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, updatable = false)
+    private Long tipo_usuario_id;
+
+    @Column(nullable = false)
+    private String tipo_usario_nome;
+    
+}
